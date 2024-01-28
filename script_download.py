433a22209e0bd3cac69c41007ed60f7aa642af09
victorhugofpdev <victorhugo.f.p.silva@gmail.com>
2024-01-28 03:08:37 -0300

    Update script_download.py

--- script_download.py
+++ script_download.py
@@ -1,18 +1,32 @@
 from pytube import YouTube
+import os
 
-# Insira o link do vídeo que você deseja baixar
-link = input("Insira o link do vídeo: ")
+def script_download():
 
-# Criando um objeto YouTube com o link
-yt = YouTube(link)
+    # Insira o link do vídeo que você deseja baixar
+    link = input("Insira o link do vídeo: ")
 
-# Acessando a stream de maior resolução disponível
-ys = yt.streams.get_highest_resolution()
+    # Criando um objeto YouTube com o link
+    yt = YouTube(link)
 
-# Solicitando ao usuário para inserir o nome do arquivo
-nome_arquivo = input("Insira o nome do arquivo sem a extensão: ")
+    # Acessando a stream de maior resolução disponível
+    ys = yt.streams.get_highest_resolution()
 
-# Iniciando o download com o nome do arquivo especificado
-print("Baixando...")
-ys.download(filename=f"{nome_arquivo}.mp4")
-print("Download concluído!")
+    # Solicitando ao usuário para inserir o nome do arquivo
+    nome_arquivo = input("Insira o nome do arquivo sem a extensão: ")
+
+    # Especificando o caminho de destino
+    caminho_destino = "C:\\Users\\VH-vscode\\Documents\\video_cortes"
+
+    # Verificando se o diretório existe, se não, cria o diretório
+    if not os.path.exists(caminho_destino):
+        os.makedirs(caminho_destino)
+
+    # Iniciando o download com o nome e caminho do arquivo especificados
+    print("Baixando...")
+    ys.download(output_path=caminho_destino, filename=f"{nome_arquivo}.mp4")
+    print("Download concluído!")
+
+    return f"{caminho_destino}/{nome_arquivo}.mp4"
+
+path = script_download()
